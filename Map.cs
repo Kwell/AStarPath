9b239ba85d705f5cc048fbfec0310dbc6e9ca086
chuanwei.wang@patchallin.com <chuanwei.wang@patchallin.com>
2017-05-11 11:30:05 +0800

    init project

---
+++ Map.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarPath
+{
+    public static class Map
+    {
+        public static int[,] TwoDimensionalaps = 
+        {
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+            { 1,1,1,1,1,1,1,1,1,1}, 
+        };
+    }
+}
